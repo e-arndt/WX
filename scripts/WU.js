cd30d0dd8d6b2ee678beb8b948d3f3a1bb63aebd
e-arndt <earndt1701@gmail.com>
2025-03-21 17:06:41 -0700

    adjust rain rates

--- scripts/WU.js
+++ scripts/WU.js
@@ -98,19 +98,19 @@ function guessCurrentCondition(observation, currentHour) {
 
     if (precipRate > 0 && precipRate <= .07) {
         condition = (temperature <= 33) ? "❄️ Light Snow" : "🌧️ Drizzling";
-    } else if (precipRate > .07 && precipRate < .12) {
+    } else if (precipRate > .07 && precipRate < .15) {
         condition = (temperature <= 33) ? "❄️ Snowing" : "🌧️ Light Rain";
-    } else if (precipRate >= .12 && precipRate < .17) {
+    } else if (precipRate >= .15 && precipRate < .20) {
         condition = (temperature <= 33) ? "❄️ Moderate Snow" : "🌧️ Raining";
-    } else if (precipRate >= .17 && precipRate <= .23) {
+    } else if (precipRate >= .20 && precipRate <= .25) {
         condition = (temperature <= 33) ? "❄️ Heavy Snow" : "🌧️ Moderate Rain";
-    } else if (precipRate >= .23 && precipRate <= .33) {
+    } else if (precipRate >= .25 && precipRate <= .35) {
         condition = (temperature <= 33) ? "❄️ Very Heavy Snow" : "🌧️ Heavy Rain";
-    } else if (precipRate >= .33 && precipRate <= .43) {
+    } else if (precipRate >= .35 && precipRate <= .45) {
         condition = (temperature <= 33) ? "❄️ Blizzard Snow" : "🌧️ Downpour";
-    } else if (precipRate >= .43 && precipRate <= .50) {
+    } else if (precipRate >= .45 && precipRate <= .60) {
         condition = (temperature <= 33) ? "❄️ Heavy Blizzard Snow" : "🌧️ Heavy Downpour";
-    } else if (precipRate >= .50) {
+    } else if (precipRate >= .61) {
         condition = (temperature <= 33) ? "❄️ Whiteout Snow" : "🌧️ Torrential Downpour";
     } else if (windSpeed > 30) {
         condition = "💨 Gale Force Winds";
